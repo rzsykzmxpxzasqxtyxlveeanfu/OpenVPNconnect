51ad6fe5339240f1d2dddc0c37d198116d74dbb8
rzsykzmxpxzasqxtyxlveeanfu <rzsykzmxpxzasqxtyxlveeanfu@users.noreply.github.com>
2017-01-02 17:25:10 +0100

    add while loop
    Add while loop, so script continues until an .ovpn file matches the regex pattern

--- openvpn-connect.sh
+++ openvpn-connect.sh
@@ -14,15 +14,19 @@ fi
 # regex pattern of file path and names:
 pattern='^(\/etc\/openvpn\/ovpn\/)(([a-z]{2})([0-9]{1,3})\.nordvpn\.com\.(udp|tcp)(1194|443)\.ovpn)$'
 
-# select a random .ovpn file
-ovpn=$(ls /etc/openvpn/ovpn/*.ovpn | shuf -n 1)
+while :
+do
 
-if [[ ! $ovpn =~ $pattern ]]
-then
-     # file does not match pattern
-     echo ERROR: File "$ovpn" does not match pattern
-     exit 1
-fi
+     # select a random .ovpn file
+     ovpn=$(ls /etc/openvpn/ovpn/*.ovpn | shuf -n 1)
+
+     if [[ $ovpn =~ $pattern ]]
+     then
+          # file matches pattern
+          break
+     fi
+
+done
 
 # display countrycode, server number, protocol and portnumber
 echo Going to open new OpenVPN connection to ${BASH_REMATCH[2]} server numer ${BASH_REMATCH[3]} over ${BASH_REMATCH[4]} though port ${BASH_REMATCH[5]}
